4b313d6f19519170eaa618cb701518a060d16038
charakamihiranga <charakavmihiranga@gmail.com>
2025-02-08 15:34:16 +0530

    add splashscreen and custom fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,34 @@
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { View } from "react-native";
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // load custom fonts
+        await Font.loadAsync({
+            "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+            "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+            "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+        });
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+    prepare();
+  }, []);
+
+
   return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Dashboard</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+    },
+    text: {
+        fontFamily: "Poppins-Regular",
+        fontSize: 18,
+    },
+});
